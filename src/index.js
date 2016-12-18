ca045c52fef1bc343be5a8dbb3b5f062031a233b
Andres Castro <crac@kirev.com>
2016-12-18 08:02:58 -0600

    feat(index.js, index.spec.js): Update the getRandom function to get a number of unique tranformers n
    Update the getRandom function to get a number of unique tranformers names

--- src/index.js
+++ src/index.js
@@ -1,7 +1,21 @@
-import transformersNames from './transformers.json';
-import uniqueRandomArray from 'unique-random-array';
+import transformersNames from './transformers.json'
+import uniqueRandomArray from 'unique-random-array'
+
+const getRandomItem = uniqueRandomArray(transformersNames)
 
 module.exports = {
-	all : transformersNames,
-	random : uniqueRandomArray(transformersNames) 
-};
+  all: transformersNames,
+  random: random
+}
+
+function random (number) {
+  if (number === undefined) {
+    return getRandomItem()
+  } else {
+    const randomItems = []
+    for (let i = 0; i < number; i++) {
+      randomItems.push(getRandomItem())
+    }
+    return randomItems
+  }
+}
--- src/index.spec.js
+++ src/index.spec.js
@@ -4,23 +4,31 @@ import transformers  from './index'
 describe('transformers-names-full', function () {
 	describe('all', function () {
 		it('should be an array of strings', function () {
-			expect(transformers.all).to.satisfy(isArrayOfStrings);
+			expect(transformers.all).to.satisfy(isArrayOfStrings)
 
 			function isArrayOfStrings (array) {
 				return array.every(function (item) {
-					return typeof item === 'string';
-				});
+					return typeof item === 'string'
+				})
 			}
-		});
-		
+		})
+
 		it('should contain `Megatron`', function () {
-			expect(transformers.all).to.include('Megatron');
-		});
-	});
+			expect(transformers.all).to.include('Megatron')
+		})
+	})
+
 	describe('random', function () {
 		it('should return a random item from the transformers.all', function () {
-			var randomItem = transformers.random();
+			var randomItem = transformers.random()
 			expect(transformers.all).to.include(randomItem)
-		});
-	});
-});
+		})
+		it('should return an array of random items if passed a number', function() {
+      var randomItems = transformers.random(3)
+      expect(randomItems).to.have.length(3)
+      randomItems.forEach(function(item) {
+        expect(transformers.all).to.include(item)
+      })
+    })
+	})
+})
